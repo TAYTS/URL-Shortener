ca3777d77840b97ffd3a706d97c065cd62d0ac13
TAY TS <taytzushieh@gmail.com>
2020-10-29 14:51:25 +0800

    Updated build app script

--- infra/build_app.sh
+++ infra/build_app.sh
@@ -3,7 +3,7 @@
 echo "Build app Docker images"
 docker-compose build mysqldb
 docker-compose build backend
-docker-compose build frontend
+docker-compose build --build-arg REACT_APP_API_URL=$REACT_APP_API_URL frontend
 
 echo "Push app Docker images to Docker hub"
 docker-compose push mysqldb
