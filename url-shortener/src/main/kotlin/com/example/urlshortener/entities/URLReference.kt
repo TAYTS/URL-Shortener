0f870796ae39826545f57f116da4bff88ca771ad
TAY TS <taytzushieh@gmail.com>
2020-12-11 21:38:30 +0800

    Update URLReference entity

--- url-shortener/src/main/kotlin/com/example/urlshortener/entities/URLReference.kt
+++ url-shortener/src/main/kotlin/com/example/urlshortener/entities/URLReference.kt
@@ -4,15 +4,15 @@ import java.time.LocalDateTime
 import javax.persistence.*
 
 
-@Entity()
+@Entity
 @Table(name = "url_reference")
 class URLReference(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "ID")
-    var id: Int,
-    @Column(name = "ORIGINAL_URL", nullable = false) var originalURL: String,
-    @Column(name = "URL_HASH", unique = true, nullable = false) var URLHash: String,
+    val id: Long? = null,
+    @Column(name = "ORIGINAL_URL", nullable = false) val originalURL: String,
+    @Column(name = "URL_HASH", unique = true, nullable = false) val URLHash: String,
     @Column(
         name = "CREATED_AT",
         nullable = false,
-    ) var createdAt: LocalDateTime = LocalDateTime.now(),
+    ) val createdAt: LocalDateTime = LocalDateTime.now(),
 )
