21203deb9d601ccbd61927e877f822900e182c43
TAY TS <taytzushieh@gmail.com>
2020-12-11 21:32:11 +0800

    Update dependencies in gradle

--- url-shortener/build.gradle.kts
+++ url-shortener/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.0"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.4.21"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
     kotlin("plugin.allopen") version "1.3.61"
@@ -28,7 +29,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.hibernate:hibernate-core")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     runtimeOnly("mysql:mysql-connector-java")
     implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
