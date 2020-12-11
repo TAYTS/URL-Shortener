93657992942a730bd4ea70f03add858cc1da161b
TAY TS <taytzushieh@gmail.com>
2020-12-11 21:39:39 +0800

    Disable jackson FAIL_ON_UNKNOWN_PROPERTIES

--- url-shortener/src/main/kotlin/com/example/urlshortener/UrlShortenerApplication.kt
+++ url-shortener/src/main/kotlin/com/example/urlshortener/UrlShortenerApplication.kt
@@ -1,11 +1,17 @@
 package com.example.urlshortener
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
 
 @SpringBootApplication
 class UrlShortenerApplication
 
 fun main(args: Array<String>) {
-	runApplication<UrlShortenerApplication>(*args)
+    val mapper = ObjectMapper()
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+    runApplication<UrlShortenerApplication>(*args)
 }
